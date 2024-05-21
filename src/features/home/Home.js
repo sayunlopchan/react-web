4f2ea75e8265a4319da97e1ddd677cf0d0f6154a
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-05-21 14:54:21 +0545

    ddd

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,67 +1,13 @@
-import { useFormik } from "formik";
-import React, { useState } from "react";
+import React,{useState} from "react";
 
 const Home = () => {
-  const [posts, setPost] = useState([]);
+  
+  const [show, setShow] = useState(false);
 
-  const formik = useFormik({
-    initialValues: {
-      post: "add todo list",
-    },
-    onSubmit: (e, { resetForm }) => {
-      setPost((prev) => [...prev, e.post]);
-      resetForm(); //reset form every submit
-    },
-  });
-
-  // delete post using slice
-  const handleDelete = (i) => {
-    const updatedPost = [...posts.slice(0, i), ...posts.slice(i + 1)];
-    setPost(updatedPost);
-  };
-
-  return (
-    <div className="m-4">
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label htmlFor="">Add info</label>
-        </div>
-        <div>
-          <input
-            onChange={formik.handleChange}
-            type="text"
-            placeholder="post add"
-            name="post"
-            value={formik.values.post} // initial value
-            className="bg-gray-300"
-          />
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
-
-      {/* maping post */}
-      {posts.map((post, i) => {
-        return (
-          <div key={i}>
-            <div className="flex gap-2 p-1">
-              <div>
-                <p className="bg-gray-300 p-1">{post}</p>
-              </div>
-              <div>
-                <button
-                  onClick={() => handleDelete(i)}
-                  className="p-1 bg-red-500"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>
+    <button onClick={() => setShow(!show)}>show</button>
+    {show && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ut animi vel sed incidunt! Excepturi consequuntur rerum vitae nesciunt voluptatum.</p>}
+  </div>;
 };
 
 export default Home;
