cb38f57ad0ea620ef4886fde40603dba15c2652c
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-05-22 15:00:38 +0545

    axios api call fetch

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,67 +1,22 @@
-import { useFormik } from "formik";
-import React, { useState } from "react";
+import React from 'react'
 
 const Home = () => {
-  const [posts, setPost] = useState([]);
 
-  const formik = useFormik({
-    initialValues: {
-      post: "add todo list",
-    },
-    onSubmit: (e, { resetForm }) => {
-      setPost((prev) => [...prev, e.post]);
-      resetForm(); //reset form every submit
-    },
-  });
+  // async await
 
-  // delete post using slice
-  const handleDelete = (i) => {
-    const updatedPost = [...posts.slice(0, i), ...posts.slice(i + 1)];
-    setPost(updatedPost);
-  };
+  console.log('brhvbhubhdsuyg')
+  
+  setTimeout(() => {
+    console.log('helllliii') // shows after 4 sec
+  }, 4000);
+  
+  console.log('brhvbhubhdsuyg')
 
   return (
-    <div className="m-4">
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label htmlFor="">Add info</label>
-        </div>
-        <div>
-          <input
-            onChange={formik.handleChange}
-            type="text"
-            placeholder="post add"
-            name="post"
-            value={formik.values.post} // initial value
-            className="bg-gray-300"
-          />
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
-
-      {/* maping post */}
-      {posts.map((post, i) => {
-        return (
-          <div key={i}>
-            <div className="flex gap-2 p-1">
-              <div>
-                <p className="bg-gray-300 p-1">{post}</p>
-              </div>
-              <div>
-                <button
-                  onClick={() => handleDelete(i)}
-                  className="p-1 bg-red-500"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
