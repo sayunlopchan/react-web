f0cfe8a49a7ebefbc4d842018f8546cb5b15425e
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-05-23 18:11:19 +0545

    getData

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,22 +1,57 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  // axios npm: npm i axios
+
+  const [data, setData] = useState([]);
+
+  // then catch
+  const getData = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        setData = res.data.products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const thenCatch = () => {
+    axios
+      .get("url")
+      .then((response) => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   // async await
+  const tryCatch = async () => {
+    try {
+      const res = await axios.get("https://dummyjson.com/products");
+      setData(res.data?.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  console.log('brhvbhubhdsuyg')
-  
-  setTimeout(() => {
-    console.log('helllliii') // shows after 4 sec
-  }, 4000);
-  
-  console.log('brhvbhubhdsuyg')
+  useEffect(() => {
+    tryCatch();
+  }, []);
 
   return (
     <div>
-      
+      {data.map((product) => {
+        return (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
